c3587841cd91be12075614439cad4ebe39ad9d05
Diego Pamplona Andrade <78365344+Diego-Pamplona@users.noreply.github.com>
2022-03-01 20:39:31 -0300

    1.5

--- server/client/src/pages/Books/index.js
+++ server/client/src/pages/Books/index.js
@@ -1,75 +1,89 @@
-import React from 'react';
-import {Link } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import {Link, useHistory } from "react-router-dom";
 import {FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
+import api from '../../services/api';
+
 
 import './styles.css';
 import logoimage from '../../assets/logo.svg'
 
 export default function Books(){
+    const [books, setBooks] = useState([]);
+    const userName = localStorage.getItem('userName');
+    const accessToken = localStorage.getItem('accessToken');
+
+    const history = useHistory();
+
+    useEffect(() => {
+        api.get('api/Book/v1/asc/10/1', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            setBooks(response.data.list)
+        })
+    }, [accessToken]);
+
+    async function logout(){
+        try {
+            await api.get('api/auth/v1/revoke', {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            localStorage.clear();
+            history.push('/');
+        } catch (error) {
+            alert('Logout failed ' + error )
+        }
+    }
+
+    async function deleteBook(id){
+        try {
+            await api.delete(`api/Book/v1/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            setBooks(books.filter(book => book.id !== id))
+        } catch (error) {
+            alert('Delete failed ' + error )
+        }
+    }
+
     return (
         <div className="book-container">
             <header>
                 <img src={logoimage} alt="Erudio"/>
-                <span>Welcome, <strong>Diego</strong>!</span>
+                <span>Welcome, <strong>{userName.toUpperCase()}</strong>!</span>
                 <Link className="button" to="book/new">Add new Book</Link>
                 <button type="button">
-                    <FiPower size={18} color="#251FC5" />
+                    <FiPower onClick={logout} size={18} color="#251FC5" />
                 </button>
             </header>
 
             <h1>Registered Books</h1>
             <ul>
-                <li>
+                {books.map(book => (
+                    <li key={book.id}>
                     <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
+                    <p>{book.title}</p>
                     <strong>Autor:</strong>
-                    <p>Nigel Poulton</p>
+                    <p>{book.author}</p>
                     <strong>Price:</strong>
-                    <p>R$ 47,90</p>
+                    <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
                     <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
+                    <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
                     
                     <button type="button">
                         <FiEdit size={20} color="#251fc5"/>
                     </button>
-                    <button type="button">
+                    <button onClick={() => deleteBook(book.id)} type="button">
                         <FiTrash2 size={20} color="#251fc5"/>
                     </button>
                 </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-                    
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Autor:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-                    
-                    <button type="button">
-                        <FiEdit size={20} color="#251fc5"/>
-                    </button>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251fc5"/>
-                    </button>
-                </li>
+                ))}
             </ul>
 
 
--- server/client/src/pages/NewBook/index.js
+++ server/client/src/pages/NewBook/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 import {Link} from 'react-router-dom'
 import {FiArrowLeft } from 'react-icons/fi';
+import api from '../../services/api';
 
 
 import './styles.css';
 import logoimage from '../../assets/logo.svg'
 
 export default function NewBook(){
+    const [author, setAuthor] = useState('');
+    const [title, setTitle] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+
+    const history = useHistory();
+
+    async function createNewBook(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            author,
+            launchDate,
+            price,
+        }
+   
+        const accessToken = localStorage.getItem('accessToken');
+
+        try {
+            await api.post('api/Book/v1', data, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+        } catch (error) {
+            alert('Erros while recorder book' + error )
+        }
+
+        history.push('/books');
+
+    }
+
     return (
         <div className="new-book-container">
             <div className="content">
@@ -19,11 +54,28 @@ export default function NewBook(){
                         Home
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Title" />
-                    <input placeholder="Autor" />
-                    <input type="date" />
-                    <input placeholder="Price" />
+                <form onSubmit={createNewBook}>
+                    <input
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <input
+                        placeholder="Autor"
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        value={launchDate}
+                        onChange={e => setLaunchDate(e.target.value)}
+                    />
+                    <input
+                        placeholder="Price"
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                    />
+
                     <button className="Button" type="submit">Add</button>
                 </form>
             </div>
